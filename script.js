5a95cfef8dde8ee2b56bd4a5f0c46663bcca1ab8
deeptigupta222 <deeptig22@navgurukul.org>
2023-06-03 12:21:53 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let a=45 ;
-console.log(a);
+// let a=45 ;
+// console.log(a);
+
+// let c=12;
+// let d=13;
+
+// let age =26; //initialisation
+
+// const name ="Deepti";
+
+// console.log("age",age)
+// age=28;;
+
+// console.log("c+d is",c+d);
+
+// console.log("age",age)
+// console.log("name",name)
+
+
+let a=23;
+
+console.log('the value of a is ',a);
