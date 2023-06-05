d2b5233b935b887cfe9cad14e5e58abb3b098dae
deeptigupta222 <deeptig22@navgurukul.org>
2023-06-05 12:54:41 +0530

    if else

---
+++ if-else.js
@@ -0,0 +1,37 @@
+const marks=62;
+
+// if (marks>90){
+//     console.log('A+');
+// }
+// else if (marks>80){
+//     console.log('A');
+// }
+// else if (marks>70){
+//     console.log('B+');
+// }
+// else if (marks>60){
+//     console.log('B');
+// }
+// else{
+//     console.log('low marks');
+// }
+
+
+
+
+const age=4;
+
+if (age>=18){
+    console.log("you are inside if");
+    console.log("you can vote");
+    if (age>60){
+        console.log("you can vote again");
+    }
+
+}
+else{
+    console.log("you can't vote");
+    if (age<5){
+        console.log("you must go to school");
+    }
+}
